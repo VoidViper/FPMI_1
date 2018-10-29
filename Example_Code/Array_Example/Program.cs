00e3b27a18f228098c334e7bca77ae6003525366
Georgi Gogov <gogigogov@abv.bg>
2018-10-29 20:42:59 +0200

    First Commit

---
+++ Example_Code/Array_Example/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numArr, currIndex = 0;
+            Console.Write("Enter the number of elements in your array: ");
+            numArr = Convert.ToInt32(Console.ReadLine());
+            int[] intArr = new int[numArr]; // zadavame nashiq array da ima broi na elementi kolkoto sme poeli ot user-a
+            int[] TimesInArr = new int[intArr.Length];
+
+            for (int i = 0; i < numArr; i++) //cikul s koito zadavame int stoinosti za broq elementi na array-a
+            {
+                Console.Write("Enter the int value for element {0}: ", i);
+                intArr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int maxVal = intArr[0];
+
+            for (int i = 1; i < intArr.Length; i++) //obhojdame masiva kakto leka jena obhojda sugar daddies za da namerim maxVal i negovata poziciq
+            {
+                if (maxVal < intArr[i])
+                {
+                    maxVal = intArr[i];
+                }
+            }
+            for (int i = 0; i < intArr.Length; i++)
+            {
+                if (intArr[i] == maxVal)
+                {
+                    TimesInArr[currIndex] = i;
+                    currIndex++; //currIndex pokazva po vsqko edno vreme v masiva kolko chisla ima
+                }
+            }
+            for (int i = 0; i < currIndex; i++)
+            {
+                Console.WriteLine("The elements with maxVal are in positions {0}", TimesInArr[i]);
+            }
+            Console.WriteLine("The highest value in the int array is: {0}", maxVal);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Example_Code/Calculator_if/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2, num3;
+            string operation;
+
+            Console.Write("num1:");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("num2:");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Operation [+,-,*,/]: ");
+            operation = Console.ReadLine();
+
+            if (operation == "+")
+            {
+                num3 = num1 + num2;
+                Console.Write($"The sum of num1 and num 2 is: {num3}", num3);
+            }
+            else if (operation == "-")
+            {
+                num3 = num1 - num2;
+                Console.Write($"By subtracting the value of num2 from num1 you get: {num3}", num3);
+            }
+            else if (operation == "*")
+            {
+                num3 = num1 * num2;
+                Console.Write($"By multiplying num1 and num2 you get : {num3}", num3);
+            }
+            else if (operation == "/")
+            {
+                num3 = num1 / num2;
+                Console.Write($"Dividing num1 and num2 gives a result of: {num3}", num3);
+            }
+            Console.ReadLine();
+        }
+    }
+}
