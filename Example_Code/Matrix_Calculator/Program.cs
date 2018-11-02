4275627296b69b8420e4ad6019eacc5e6c294896
Georgi Gogov <gogigogov@abv.bg>
2018-11-02 18:44:22 +0200

    added "Do you want to continue? [y/n]" added square matrix multiplication

--- Example_Code/Matrix_Calculator/Program.cs
+++ Example_Code/Matrix_Calculator/Program.cs
@@ -8,121 +8,178 @@ namespace Matrix_Calculator
         static void Main(string[] args)
         {
             int rowsA, rowsB, colA, colB;
+            int sum = 0;
             string operation;
+            bool doContinue = false;
+
             Console.WriteLine("This program adds, subtracts and multiplies matrices. If you enter a square matrix");
             Console.WriteLine("you will be able to add, subtract and multiply. If not, you will only see the matrices you've entered.");
             Console.WriteLine();
-            Console.Write("Write the number of rows in Matrix A:");
-            rowsA = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Write the number of cols in Matrix A:");
-            colA = Convert.ToInt32(Console.ReadLine());
-            int[,] matrixA = new int[rowsA, colA];
 
-            for (int i = 0; i < rowsA; i++)
+            do
             {
-                for (int j = 0; j < colA; j++)
-                {
-                    Console.Write($"Enter element ({i+1},{j+1}): ");
-                    matrixA[i, j] = Convert.ToInt32(Console.ReadLine());
-                }
-            }
+                Console.Write("Write the number of rows in Matrix A:");
+                rowsA = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Write the number of cols in Matrix A:");
+                colA = Convert.ToInt32(Console.ReadLine());
+                int[,] matrixA = new int[rowsA, colA];
 
-            Console.WriteLine("MatrixA is: ");
-            for (int i = 0; i < rowsA; i++)
-            {
-                for (int j = 0; j < colA; j++)
+                for (int i = 0; i < rowsA; i++)
                 {
-                    Console.Write(" " + matrixA[i, j] + " ");
+                    for (int j = 0; j < colA; j++)
+                    {
+                        Console.Write($"Enter element ({i + 1},{j + 1}): ");
+                        matrixA[i, j] = Convert.ToInt32(Console.ReadLine());
+                    }
                 }
-                Console.WriteLine();
-            }
 
-            Console.Write("Write the number of rows in Matrix B:");
-            rowsB = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Write the number of cols in Matrix B:");
-            colB = Convert.ToInt32(Console.ReadLine());
-            int[,] matrixB = new int[rowsB, colB];
-            for (int i = 0; i < rowsB; i++)
-            {
-                for (int j = 0; j < colB; j++)
+                Console.WriteLine("MatrixA is: ");
+                for (int i = 0; i < rowsA; i++)
                 {
-                    Console.Write($"Enter element ({i+1},{j+1}): ");
-                    matrixB[i, j] = Convert.ToInt32(Console.ReadLine());
+                    for (int j = 0; j < colA; j++)
+                    {
+                        Console.Write(" " + matrixA[i, j] + " ");
+                    }
+                    Console.WriteLine();
                 }
-            }
 
-            Console.WriteLine("MatrixB is: ");
-            for (int i = 0; i < rowsB; i++)
-            {
-                for (int j = 0; j < colB; j++)
+                Console.Write("Write the number of rows in Matrix B:");
+                rowsB = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Write the number of cols in Matrix B:");
+                colB = Convert.ToInt32(Console.ReadLine());
+                int[,] matrixB = new int[rowsB, colB];
+                for (int i = 0; i < rowsB; i++)
                 {
-                    Console.Write(" " + matrixB[i, j] + " ");
+                    for (int j = 0; j < colB; j++)
+                    {
+                        Console.Write($"Enter element ({i + 1},{j + 1}): ");
+                        matrixB[i, j] = Convert.ToInt32(Console.ReadLine());
+                    }
                 }
-                Console.WriteLine();
-            }
-            int[,] Buffer = new int[Math.Max(rowsA, rowsB), Math.Max(colA, colB)];
-            int n = colA;
-            Console.WriteLine("What operation would you like to perform? (+,-,*)");
-            operation = Console.ReadLine();
-            if (rowsA != colA && rowsB != colB && colA == rowsB)
-            {
-                if (operation == "*")
+
+                Console.WriteLine("MatrixB is: ");
+                for (int i = 0; i < rowsB; i++)
                 {
-                    Console.WriteLine("Multiplication phase 1");
+                    for (int j = 0; j < colB; j++)
+                    {
+                        Console.Write(" " + matrixB[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                int[,] Buffer = new int[Math.Max(rowsA, rowsB), Math.Max(colA, colB)];
+                int n = colA;
+                Console.WriteLine("What operation would you like to perform? (+,-,*)");
+                operation = Console.ReadLine();
+                if (rowsA != colA && rowsB != colB && colA == rowsB)
+                {
+                    if (operation == "*")
+                    {
+                        /*for (int i = 0; i < rowsA; i++)
+                            for (int j = 0; j < colB; j++)
+                                Buffer[i, j] = 0;
+                        for (int i = 0; i < rowsA; i++)    
+                        {
+                            for (int j = 0; j < colB; j++)    
+                            {
+                                sum = 0;
+                                for (int k = 0; k < colA; k++)
+                                    sum = sum + matrixA[i, k] * matrixB[k, j];
+                                Buffer[i, j] = sum;
+                            }
+                        }
+                        for (int i = 0; i < n; i++)
+                        {
+                            Console.Write("");
+                            for (int j = 0; j < n; j++)
+                                Console.Write($" {Buffer[i, j]} ");
+                            Console.WriteLine();
+                        }*/
+                        Console.WriteLine("Not square matrix multiplication WIP");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid operation!");
+                    }
+                }
+                else if (rowsA == colA && rowsB == colB)
+                {
+                    //Thanks to Mincho da brain
+                    if (operation == "+")
+                    {
+                        Console.WriteLine("Addition");
+                        for (int i = 0; i < n; i++)
+                        {
+                            for (int j = 0; j < n; j++)
+                            {
+                                Buffer[i, j] = matrixA[i, j] + matrixB[i, j];
+                            }
+                        }
+                        for (int i = 0; i < n; i++)
+                        {
+                            Console.Write("");
+                            for (int j = 0; j < n; j++)
+                                Console.Write($" {Buffer[i, j]} ");
+                            Console.WriteLine();
+                        }
+                    }
+                    if (operation == "-")
+                    {
+                        Console.WriteLine("Subtraction");
+                        for (int i = 0; i < n; i++)
+                        {
+                            for (int j = 0; j < n; j++)
+                            {
+                                Buffer[i, j] = matrixA[i, j] - matrixB[i, j];
+                            }
+                        }
+                        for (int i = 0; i < n; i++)
+                        {
+                            Console.Write("");
+                            for (int j = 0; j < n; j++)
+                                Console.Write($" {Buffer[i, j]} ");
+                            Console.WriteLine();
+                        }
+                    }
+                    if (operation == "*")
+                    {
+                        for (int i = 0; i < rowsA; i++)
+                            for (int j = 0; j < colB; j++)
+                                Buffer[i, j] = 0;
+                        for (int i = 0; i < rowsA; i++)
+                        {
+                            for (int j = 0; j < colB; j++)
+                            {
+                                sum = 0;
+                                for (int k = 0; k < colA; k++)
+                                    sum = sum + matrixA[i, k] * matrixB[k, j];
+                                Buffer[i, j] = sum;
+                            }
+                        }
+                        for (int i = 0; i < n; i++)
+                        {
+                            Console.Write("");
+                            for (int j = 0; j < n; j++)
+                                Console.Write($" {Buffer[i, j]} ");
+                            Console.WriteLine();
+                        }
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("Invalid operation!");
+                    Console.WriteLine("Error! Cannot do operations on these matrices. Exiting.");
                 }
-            }
-            else if (rowsA == colA && rowsB == colB)
-            {
-                //Thanks to Mincho da brain
-                if (operation == "+")
+
+                Console.Write("Do you want to continue? [y/n]: ");
+                if (Console.ReadLine() == "y")
                 {
-                    Console.WriteLine("Addition");
-                    for (int i = 0; i < n; i++)
-                    {
-                        for (int j = 0; j < n; j++)
-                        {
-                            Buffer[i, j] = matrixA[i, j] + matrixB[i, j];
-                        }
-                    }
-                    for (int i = 0; i < n; i++)
-                    {
-                        Console.Write("");
-                        for (int j = 0; j < n; j++)
-                            Console.Write($" {Buffer[i, j]} ");
-                        Console.WriteLine();
-                    }
+                    doContinue = true;
                 }
-                if (operation == "-")
-                {
-                    Console.WriteLine("Subtraction");
-                    for (int i = 0; i < n; i++)
-                    {
-                        for (int j = 0; j < n; j++)
-                        {
-                            Buffer[i, j] = matrixA[i, j] - matrixB[i, j];
-                        }
-                    }
-                    for (int i = 0; i < n; i++)
-                    {
-                        Console.Write("");
-                        for (int j = 0; j < n; j++)
-                            Console.Write($" {Buffer[i, j]} ");
-                        Console.WriteLine();
-                    }
-                }
-                if (operation == "*")
-                {
-                    Console.WriteLine("Multiplication phase 2");
-                }
+                else doContinue = false;
             }
-            else
-            {
-                Console.WriteLine("Error! Cannot do operations on these matrices. Exiting.");
-            }
+            while (doContinue == true);
+            
+           
+            
         }
     }
 }
