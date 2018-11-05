20213249fc7509790c26e70607d9b015d5b8b024
Georgi Gogov <gogigogov@abv.bg>
2018-11-05 16:36:38 +0200

    cows and bulls

---
+++ Example_Code/Cows_and_Bulls/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cows_and_Bulls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //cows = dadeana cifra sushtestvuva v chisloto
+            //bulls = dadenata cifra sushtestvuva i e na pravilnoto mqsto
+            string numToGuess, guess;
+            int bulls = 0, cows = 0;
+
+            Console.Write("Enter a number to be guessed: ");
+            numToGuess = Console.ReadLine();
+            do
+            {
+                bulls = 0;
+                cows = 0;
+                Console.Write("Enter your guess: ");
+                guess = Console.ReadLine();
+                for (int i = 0; i < numToGuess.Length; i++)
+                {
+                    for (int j = 0; j < guess.Length; j++)
+                    {
+                        if ((numToGuess[i] == guess[j]) && (i == j))
+                        {
+                            bulls++;
+                        }
+                        else if (numToGuess[i] == guess[j])
+                        {
+                            cows++;
+                        }
+                    }
+                }
+                Console.WriteLine($"{bulls} bulls");
+                Console.WriteLine($"{cows} cows");
+            }
+            while (bulls != numToGuess.Length);
+        }
+    }
+}
--- Example_Code/TestCode/Program.cs
+++ Example_Code/TestCode/Program.cs
@@ -10,75 +10,9 @@ namespace TestCode
     {
         static void Main(string[] args)
         {
-            int[,] arrA, arrB, buffer;
-            int rowsA, rowsB, colA, colB;
-            Console.Write("RowsA:");
-            rowsA = Convert.ToInt32(Console.ReadLine());
-            Console.Write("ColA:");
-            colA = Convert.ToInt32(Console.ReadLine());
-            arrA = new int[rowsA, colA];
-            for (int i = 0; i < rowsA; i++)
-            {
-                for (int j = 0; j < colA; j++)
-                {
-                    Console.Write($"Enter element [{i},{j}]");
-                    arrA[i, j] = Convert.ToInt32(Console.ReadLine());
-                }
-            }
-            Console.WriteLine("Matrix A is:");
-            for (int i = 0; i < rowsA; i++)
-            {
-                for (int j = 0; j < colA; j++)
-                {
-                    Console.Write(" " + arrA[i,j] + " ");
-                    
-                }
-                Console.WriteLine();
-            }
-
-            Console.Write("RowsB:");
-            rowsB = Convert.ToInt32(Console.ReadLine());
-            Console.Write("ColB:");
-            colB = Convert.ToInt32(Console.ReadLine());
-            arrB = new int[rowsB, colB];
-            for (int i = 0; i < rowsB; i++)
-            {
-                for (int j = 0; j < colB; j++)
-                {
-                    Console.Write($"Enter element [{i},{j}]");
-                    arrB[i, j] = Convert.ToInt32(Console.ReadLine());
-                }
-            }
-            Console.WriteLine("Matrix B is:");
-            for (int i = 0; i < rowsB; i++)
-            {
-                for (int j = 0; j < colB; j++)
-                {
-                    Console.Write(" " + arrB[i,j] + " ");
-                }
-                Console.WriteLine();
-            }
-            buffer = new int[Math.Max(rowsA, rowsB), Math.Max(colA, colB)];
-            Console.WriteLine("Matrix A Transposed is:");
-            for (int i = 0; i < colA; i++)
-            {
-                for (int j = 0; j < rowsA; j++)
-                {
-                    Console.Write(" " + arrA[j, i] + " ");
-                }
-                Console.WriteLine();
-            }
-
-            Console.WriteLine("Matrix B Transposed is:");
-            for (int i = 0; i < colB; i++)
-            {
-                for (int j = 0; j < rowsB; j++)
-                {
-                    Console.Write(" " + arrB[j, i] + " ");
-                }
-                Console.WriteLine();
-            }
-
+            string a = "asdasd";
+            string b= "asdasd";
+            Console.WriteLine(a[0]==b[0]);
         }
     }
 
