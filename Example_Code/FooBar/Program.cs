632b2830b1b5c50523ef617da6de44021650e879
Georgi Gogov <gogigogov@abv.bg>
2018-11-07 17:50:14 +0200

    new

---
+++ Example_Code/FooBar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FooBar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            Console.Write("Enter a number: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                if(( i % 3 == 0) && (i % 5 == 0))
+                {
+                    Console.WriteLine("FooBar");
+                }
+                else if (i % 3 == 0) Console.WriteLine("Foo");
+                else if (i % 5 == 0) Console.WriteLine("Bar");
+                else Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Example_Code/VendingMachine_Resto/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine_Resto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //moneti - 1, 2, 5, 10, 20, 50, 100
+            double resto = 0; //restoto
+            int monetiAmount = 0;
+            Console.Write("Enter the resto: ");
+            resto = Convert.ToDouble(Console.ReadLine());
+            while (resto > 0)
+            {
+                
+                if(resto >= 2.0d) resto -= 2.0d;             
+                else if (resto >= 1.0d) resto -= 1.0d;                
+                else if (resto >= 0.5d) resto -= 0.5d;               
+                else if (resto >= 0.2d) resto -= 0.2d;                
+                else if (resto >= 0.1d) resto -= 0.1d;               
+                else if (resto >= 0.05d) resto -= 0.05d;                
+                else if (resto >= 0.02d) resto -= 0.02;
+                else if (resto >= 0.01d) resto -= 0.01;
+                
+                monetiAmount++;
+            }
+            Console.WriteLine($"Minimum amount of coins is: {monetiAmount}");
+        }
+    }
+}
