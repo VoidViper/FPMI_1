ccadeace6a4cb99ba72ea17208e931024df10fa4
Georgi Gogov <gogigogov@abv.bg>
2018-11-17 19:11:55 +0200

    asd

---
+++ Example_Code/Aylinka123/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aylinka123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = new int[16];
+            double sum = 0, avg = 0;
+            int belowAvg = 0, counter = 0, aboveAvg = 0;
+
+            for (int i = 0; i < 16; i++)
+            {
+                scores[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < 16; i++)
+            {
+                sum += scores[i];
+            }
+            avg = sum / 16;
+
+            for (int i = 0; i < 16; i++)
+            {
+                if (scores[i] <= avg)
+                {
+                    belowAvg++;
+                }
+                else aboveAvg++;
+            }            
+
+            Console.WriteLine($"Average: {avg}");
+            Console.WriteLine($"Below Average: {belowAvg} scores");
+            Console.WriteLine($"Avobe Average: {aboveAvg} scores");
+
+
+
+        }
+    }
+}
