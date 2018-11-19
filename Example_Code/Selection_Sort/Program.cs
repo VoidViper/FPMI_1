834ae0bea5d54b35cd8792472d2370d58843d947
Georgi Gogov <gogigogov@abv.bg>
2018-11-20 00:28:21 +0200

    asd

---
+++ Example_Code/Selection_Sort/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selection_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr, buffer;
+            
+            Console.Write("Enter the amount of elements in the array: ");
+            arr = new int[int.Parse(Console.ReadLine())];
+            buffer = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if(arr[i] <= arr[i+1])
+                {
+                    buffer[i] = arr[i];
+                }
+            }
+        }
+    }
+}
