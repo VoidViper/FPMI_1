c776e05ae9e3fcb490366277f7b59360fcc33f6f
Georgi Gogov <gogigogov@abv.bg>
2018-12-09 19:56:48 +0200

    asd

--- Moodle/n-ti_chlen/Program.cs
+++ Moodle/n-ti_chlen/Program.cs
@@ -25,55 +25,51 @@ namespace n_ti_chlen
                 Console.WriteLine(a[i]);
             }
         }
-        static void RecursiveFunction(int n)
+        static void RecursiveFunction(int n, int A1, int A2, int An)
         {
-            Console.WriteLine("Recursive:");
-            int[] a = new int[n];
-            a[1] = 2;
-            a[2] = 4;
-            a[3] = 6;
-            
+            int val = An;
+            An = (3 * A1) + (4 * A2) - (7 * An);
+            A1 = A2;
+            A2 = val;
+            n--;
+            if (n == 3)
+            {
+                Console.WriteLine(An);
+                return;
+            }
+            RecursiveFunction(n, A1, A2, An);
         }
         static void Main(string[] args)
         {
             //Ai = 3 * Ai-3 + 4 * Ai-2 - 7 * Ai-1
             //A1 = 2, A2 = 4, A3 = 6
-            bool doContinue = true;
-            do
+            Console.Write("Enter how many elements you would like to calculate (more than 3): ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("Do you want to run the irrative funciton(1) or the recursive function(2)[e.g 1]: ");
+            int p = int.Parse(Console.ReadLine());
+            if (p == 1)
             {
-                Console.Write("Enter how many elements you would like to calculate (more than 3): ");
-                int n = int.Parse(Console.ReadLine());
-                Console.WriteLine("Do you want to run the irrative funciton(1) or the recursive function(2)[e.g]:1");
-                int p = int.Parse(Console.ReadLine());
-                if (p == 1)
+                if (n >= 3)
                 {
-                    if (n >= 3)
-                    {
-                        IterativeFunction(n);
-                        doContinue = false;
-                    }
-                    else
-                    {
-                        Console.WriteLine("The amount you have entered is not greater than or equal to 3. Please try again. ");
-                    }
+                    IterativeFunction(n);
                 }
-                else if (p == 2)
+                else
                 {
-                    if (n >= 3)
-                    {
-                        RecursiveFunction(n);
-                        doContinue = false;
-                    }
-                    else
-                    {
-                        Console.WriteLine("The amount you have entered is not greater than or equal to 3. Please try again. ");
-                    }
+                    Console.WriteLine("The amount you have entered is not greater than or equal to 3. Please try again. ");
                 }
-                else Console.WriteLine("Invalid input. Try again.");
             }
-            while (doContinue);
-            
-            
+            else if (p == 2)
+            {
+                if (n >= 3)
+                {
+                    RecursiveFunction(n, 2, 4, 6);
+                }
+                else
+                {
+                    Console.WriteLine("The amount you have entered is not greater than or equal to 3. Please try again. ");
+                }
+            }
+            else Console.WriteLine("Invalid input.");
         }
     }
 }
